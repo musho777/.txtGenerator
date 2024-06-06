8c6fde9288d42a672472f763cee1ecc079ac6e1a
musho777 <129147695+musho777@users.noreply.github.com>
2024-06-07 01:30:43 +0400

    change name

--- android/app/src/main/java/com/Photo.Stars/MainActivity.kt
+++ android/app/src/main/java/com/Photo.Stars/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.photos
+package com.Photo.Star
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/Photo.Stars/MainApplication.kt
+++ android/app/src/main/java/com/Photo.Stars/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.photos
+package com.Photo.Star
 
 import android.app.Application
 import com.facebook.react.PackageList
